1d17de23dabe035a83117b470ea0491d48138800
ozyhale <ozyhale2@gmail.com>
2023-10-18 09:43:12 +0800

    created a function that will handle login submission

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,10 @@
 import React from 'react'
 
+async function login(data: FormData){
+    "use server"
+    console.log('test');
+}
+
 const LoginPage = () => {
     return (
         <>
@@ -7,7 +12,7 @@ const LoginPage = () => {
                 <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
                     <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                         <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                            <div className="space-y-4 md:space-y-6">
+                            <form action={login} className="space-y-4 md:space-y-6">
                                 <div>
                                     <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
                                     <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
@@ -17,9 +22,9 @@ const LoginPage = () => {
                                     <input type="password" name="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
                                 </div>
                                 
-                                <button type="button" className="btn btn-primary">Log in</button>
+                                <button type="submit" className="btn btn-primary">Log in</button>
                                 
-                            </div>
+                            </form>
                         </div>
                     </div>
                 </div>
