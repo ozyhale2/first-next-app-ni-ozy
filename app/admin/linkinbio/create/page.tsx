86782df8c8063544764411d2c8e9a8aa2fdc4fc2
ozyhale <ozyhale2@gmail.com>
2023-10-19 17:56:37 +0800

    create making the slug auto generated

--- app/admin/linkinbio/create/page.tsx
+++ app/admin/linkinbio/create/page.tsx
@@ -1,18 +1,31 @@
 import React from 'react'
 import { z } from 'zod'
 import { PrismaClient } from '@prisma/client'
+import { getServerSession } from 'next-auth';
 
-const schema = z.coerce.string();
+const zCoerceStr = z.coerce.string();
+const zNum = z.number();
 const prisma = new PrismaClient();
 
 async function createLinkInBio(data: FormData) {
     'use server'
 
-    const name = schema.parse(data.get('name'))
-    const description = schema.parse(data.get('description'))
-    const slug = schema.parse(data.get('slug'))
+    const session = await getServerSession();
 
-    console.log({name, description, slug})
+    const user = await prisma.user.findUnique({
+        where: {
+            email: zCoerceStr.parse(session?.user?.email)
+        }
+    });
+
+    if(user){
+        const name = zCoerceStr.parse(data.get('name'))
+        const description = zCoerceStr.parse(data.get('description'))
+        const userId = zNum.parse(user.id)
+
+        console.log({name, description, userId});
+    }
+    
 }
 
 const LinkInBioCreatePage = () => {
@@ -39,26 +52,11 @@ const LinkInBioCreatePage = () => {
                     id="description"
                     className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                     placeholder=" "
-                    required
                 />
                 <label
                     htmlFor="description"
                     className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
                 >Description</label>
-            </div>
-            <div className="relative z-0 w-full mb-6 group">
-                <input
-                    type="slug"
-                    name="slug"
-                    id="slug"
-                    className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                    placeholder=" "
-                    required
-                />
-                <label
-                    htmlFor="slug"
-                    className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-                >Slug</label>
             </div>
             <button type="submit" className="btn btn-primary">Submit</button>
         </form>
