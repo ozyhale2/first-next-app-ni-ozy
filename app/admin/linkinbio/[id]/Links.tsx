1ccc3d314a3c25b01792cb750a579c16f458b037
ozyhale <ozyhale2@gmail.com>
2023-10-25 09:05:14 +0800

    making the list for link feature

--- app/admin/linkinbio/[id]/Links.tsx
+++ app/admin/linkinbio/[id]/Links.tsx
@@ -1,10 +1,48 @@
 import CreateUI from "./CreateUI"
+import { PrismaClient } from '@prisma/client';
 
-const Links = async (props: {linkInBioId: number}) => {
+const prisma = new PrismaClient();
+
+const Links = async (props: { linkInBioId: number }) => {
+
+    const linkRecords = await prisma.links.findMany({
+        where: {
+          deleted: false,
+          linkInBioId: props.linkInBioId
+        }
+      });
+
     return (
         <>
-            <CreateUI linkInBioId={props.linkInBioId}  />
-            <div>Links</div>
+            <CreateUI linkInBioId={props.linkInBioId} />
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>Name</th>
+                            <th>Link</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        {linkRecords.map((linkRecord) => {
+                            return <tr key={linkRecord.id} className="hover">
+                                <th>{linkRecord.id}</th>
+                                <td>{linkRecord.name}</td>
+                                <td>{linkRecord.link}</td>
+                                <td>
+                                    <button className="btn btn-error btn-xs mr-1" type="button">Delete</button>
+                                    <button className="btn btn-info btn-xs" type="button">Edit</button>
+                                </td>
+                            </tr>
+                        })}
+                        
+                    </tbody>
+                </table>
+            </div>
         </>
     )
 }
